a26a577b1198bbc5267aa559292477e9667dd1be
suparn32 <58909011+suparn32@users.noreply.github.com>
2024-01-31 03:43:46 -0600

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,8 @@
-# To create unit tests for the calculator program, we'll employ the unittest framework, a built-in component of Python's standard library. 
+# To create unit tests for the calculator program, we'll employ the pytest framework, a built-in component of Python's standard library. 
 # These tests aim to validate the functionality of each calculator function (addition, subtraction, multiplication, division) and ensure 
 # their proper execution across different scenarios.
 
-import unittest
+import pytest
 from calculator import add 
 from calculator import subtract
 from calculator import multiply
@@ -15,51 +15,51 @@ from calculator import divide
 
 
 
-class TestCalculatorFunctions(unittest.TestCase):
+class TestCalculatorFunctions():
 
     def test_addition(self):
         result = add(4, 5)
-        self.assertEqual(result, 9)
+        assert result == 9
         result = add(-4, 5)
-        self.assertEqual(result, 1)
+        assert result == 1 
         result = add(-7, -3)
-        self.assertEqual(result,-10)
+        assert result ==-10
         
 
     def test_subtraction(self):
         result = subtract(8, 3)
-        self.assertEqual(result, 5)
+        assert result == 5
         result = subtract(-8, 3)
-        self.assertEqual(result, -11)
+        assert result == -11
         result = subtract(8, -3)
-        self.assertEqual(result, 11)
+        assert result == 11
         result = subtract(-8, -3)
-        self.assertEqual(result, -5)
+        assert result == -5
 
     def test_multiplication(self):
         result = multiply(4, 6)
-        self.assertEqual(result, 24)
+        assert result == 24
         result = multiply(-4, 6)
-        self.assertEqual(result, -24)
+        assert result == -24
         result = multiply(4, -6)
-        self.assertEqual(result, -24)
+        assert result == -24
         result = multiply(-4, -6)
-        self.assertEqual(result, 24)
+        assert result == 24
 
     def test_division(self):
         result = divide(9, 3)
-        self.assertEqual(result, 3)
+        assert result == 3
         result = divide(-9, 3)
-        self.assertEqual(result, -3)
+        assert result == -3
         result = divide(9, -3)
-        self.assertEqual(result, -3)
+        assert result == -3
         result = divide(-9, -3)
-        self.assertEqual(result, 3)
+        assert result == 3
         result = divide(0, 3)
-        self.assertEqual(result, 0)
+        assert result == 0
 
         result = divide(10, 0)
-        self.assertEqual(result, "Infinite result !! Division by zero NOT POSSIBLE")
+        assert result == "Infinite result !! Division by zero NOT POSSIBLE"
 
 
 # Over here the tests defined below i:e 
@@ -68,23 +68,22 @@ class TestCalculatorFunctions(unittest.TestCase):
 #   input for the calculator operation
 
     def test_non_numeric_input_expected_exception(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             add("abc", 5)
     
     def test_multiply_non_numeric_input_expected_exception(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             multiply("abc", 5)
 
     def test_subtract_non_numeric_input_expected_exception(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             subtract("abc", 5)
     
-    
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+   pytest.main() 
+
 
 ## To run the tests we have to make sure that the test_calculator.py , calculator.py and calculator_f.py are in the same directory otherwise
 ## we would have to provide the specific path where these files are. TO RUN THE TESTS WE CAN WRITE A COMMAND IN THE TERMINAL 
-
-## bash
-## python -m unittest test_calculator.py. This command would trigger the unit tests and we would be able to see if the tests passed or failed.
+## pytest test_calculator.py This command would trigger the unit tests and we would be able to see if the tests passed or failed.
+## The no of dots specified let us know the number of tests passed.
